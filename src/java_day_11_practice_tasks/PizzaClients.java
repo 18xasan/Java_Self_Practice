6e6462c6eb61264e7d05ed19b6adac1679c7aecf
Khasan <kyuldashev2000@icloud.com>
2024-02-04 20:17:01 -0500

    Custom Class Part 2 Practice Tasks

---
+++ src/java_day_11_practice_tasks/PizzaClients.java
@@ -0,0 +1,20 @@
+package java_day_11_practice_tasks;
+
+public class PizzaClients {
+
+    public static void main(String[] args) {
+
+
+        Pizza pizza1 = new Pizza("large", 0, 1);
+        System.out.println(pizza1);
+
+        Pizza pizza2 = new Pizza("small", 4, 2);
+        System.out.println(pizza2);
+
+        Pizza pizza3 = new Pizza("medium", 3, 4);
+        System.out.println(pizza3);
+
+
+    }
+}
+
