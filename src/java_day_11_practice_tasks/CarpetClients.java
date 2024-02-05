0e66856c68acec6bb054ef4d13259749c9f3d638
Khasan <kyuldashev2000@icloud.com>
2024-02-04 20:39:21 -0500

    Custom Class Part 2 Practice Tasks

---
+++ src/java_day_11_practice_tasks/CarpetClients.java
@@ -0,0 +1,14 @@
+package java_day_11_practice_tasks;
+
+public class CarpetClients {
+
+    public static void main(String[] args) {
+
+        Carpet carpet1 = new Carpet(3.5, 5.5, 2.5, false);
+        System.out.println(carpet1);
+
+        Carpet carpet2 = new Carpet(6.5, 7.5, 3.5, true);
+        System.out.println(carpet2);
+
+    }
+}
