40ef2ececa73c3ddc56c0217583b1e0387c63fff
Khasan <kyuldashev2000@icloud.com>
2024-02-05 16:47:17 -0500

    Encapsulation Practice Tasks

---
+++ src/java_day_12_practice_tasks/CircleClients.java
@@ -0,0 +1,22 @@
+package java_day_12_practice_tasks;
+
+public class CircleClients {
+
+    public static void main(String[] args) {
+
+        Circle circle1 = new Circle(7);
+        System.out.println(circle1);
+        System.out.println(circle1.getRadius());
+        System.out.println(circle1.calcPerimeter());
+        System.out.println(circle1.calcArea());
+
+        System.out.println();
+
+        Circle circle2 = new Circle(3);
+        System.out.println(circle2);
+        System.out.println(circle2.getRadius());
+        System.out.println(circle2.calcPerimeter());
+        System.out.println(circle2.calcArea());
+
+    }
+}
