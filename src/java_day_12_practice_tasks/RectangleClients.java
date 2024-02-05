94df8b03e0e547f60d6d2b8cbe43e9d9c2aec3db
Khasan <kyuldashev2000@icloud.com>
2024-02-05 17:09:14 -0500

    Encapsulation Practice Tasks

---
+++ src/java_day_12_practice_tasks/RectangleClients.java
@@ -0,0 +1,26 @@
+package java_day_12_practice_tasks;
+
+
+public class RectangleClients {
+
+    public static void main(String[] args) {
+
+        Rectangle rectangle1 = new Rectangle(4, 6);
+        System.out.println(rectangle1.calcArea());
+        System.out.println(rectangle1.calcPerimeter());
+        System.out.println(rectangle1.getLength());
+        System.out.println(rectangle1.getWidth());
+        System.out.println(rectangle1);
+
+        System.out.println();
+
+        Rectangle rectangle2 = new Rectangle(6, 10);
+        System.out.println(rectangle2.calcArea());
+        System.out.println(rectangle2.calcPerimeter());
+        System.out.println(rectangle2.getLength());
+        System.out.println(rectangle2.getWidth());
+        System.out.println(rectangle2);
+
+
+    }
+}
