7a85703b969ed984afd6bc3ac1be0ad2a65518ca
Khasan <kyuldashev2000@icloud.com>
2024-02-04 20:17:58 -0500

    Wrapper Class and ArrayList Practice Tasks

--- src/java_day_10/CombineTwoArrays.java
+++ src/java_day_10/CombineTwoArrays.java
@@ -18,3 +18,15 @@ public class CombineTwoArrays {
         System.out.println("list ==> " + list);
     }
 }
+
+/*
+4. Create a class called CombineTwoArrays and write a program with the following specifications:
+   4.1 Given two String arrays, combine them into one ArrayList.
+
+                Example:
+                    arr1 = {"A", "B", "C"};
+                    arr2 = {"D", "E", "F", "G"};
+
+                Output:
+                    list ==> {"A", "B", "C", "D", "E", "F", "G"}
+ */
